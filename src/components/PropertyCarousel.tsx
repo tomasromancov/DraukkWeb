a8aafdb1f95f40dadf0638088816c9267a4c85da
tomasromancov <tomas.romancov@qubicstudio.com>
2024-09-28 12:23:55 +0100

    Added custom arrows to the Splide Carousel

--- src/components/PropertyCarousel.tsx
+++ src/components/PropertyCarousel.tsx
@@ -1,7 +1,6 @@
 import { Box, SvgIcon } from "@mui/material";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import { Colors } from "../ts/Colors.ts";
 
@@ -11,31 +10,57 @@ interface Props {
 
 function PropertyCarousel({ images }: Props) {
   return (
-    <Splide aria-labell="Splide Carousel" options={{ rewind: true }}>
-      {images.map((image, index) => (
-        <SplideSlide>
-          <Box
-            sx={{
-              width: "100%",
-              height: "100%",
-              borderRadius: "12px",
-              overflow: "hidden",
-              backgroundColor: "#000000",
-            }}
-          >
-            <img
-              key={index}
-              src={image}
-              alt={"Image " + index}
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "contain",
-              }}
+    <Splide
+      hasTrack={false}
+      aria-labell="Splide Carousel"
+      options={{ rewind: true }}
+    >
+      <Box>
+        <SplideTrack style={{ height: "100%" }}>
+          {images.map((image, index) => (
+            <>
+              <SplideSlide>
+                <Box
+                  sx={{
+                    width: "100%",
+                    height: "100%",
+                    overflow: "hidden",
+                  }}
+                >
+                  <img
+                    key={index}
+                    src={image}
+                    alt={"Image " + index}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "contain",
+                    }}
+                  />
+                </Box>
+              </SplideSlide>
+            </>
+          ))}
+        </SplideTrack>
+        {/* Custom arrows for the carousel.
+          Splide flips the prev arrow around so the same 
+          icon can be used for prev and next.
+        */}
+        <div className="splide__arrows">
+          <button className="splide__arrow splide__arrow--prev">
+            <SvgIcon
+              sx={{ fontSize: "large", fill: "#000000" }}
+              component={NavigateNextIcon}
             />
-          </Box>
-        </SplideSlide>
-      ))}
+          </button>
+          <button className="splide__arrow splide__arrow--next">
+            <SvgIcon
+              sx={{ fontSize: "large", fill: "#000000" }}
+              component={NavigateNextIcon}
+            />
+          </button>
+        </div>
+      </Box>
     </Splide>
   );
 }
