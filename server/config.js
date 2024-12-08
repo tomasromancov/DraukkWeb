cd92058e2f31d4b17307f6ff0d369c6ffb2b297f
tomasromancov <tomas.romancov@gmail.com>
2024-12-08 13:39:45 +0000

    deleted server folder and all relevant files - not needed for static implementation

--- server/config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-const mysql = require("mysql2");
-
-const urlDB = `mysql://root:nXLWRcYoYfsHoWhvnZiGayNWwHgbitqQ@mysql.railway.internal:3306/railway`
-const connection = mysql.createConnection(urlDB);
-
-module.exports = connection;
